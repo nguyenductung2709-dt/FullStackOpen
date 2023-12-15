c7ef9169debcd5ce3e534f7418444aa7211637b3
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2023-12-15 17:35:40 +0200

    2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -4,11 +4,14 @@ import axios from 'axios';
 const App = () => {
   const [newText, setNewText] = useState('');
   const [countries, setCountries] = useState([]);
-  const [selectedCountry, setSelectedCountry] = useState(null); 
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
+
+  const api_key = import.meta.env.VITE_SOME_KEY;
 
   const handleNewText = (event) => {
     setNewText(event.target.value);
-    setSelectedCountry(null); 
+    setSelectedCountry(null);
   };
 
   useEffect(() => {
@@ -20,16 +23,35 @@ const App = () => {
         setCountries(response.data);
       })
       .catch((error) => {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching countries data:', error);
       });
   }, []);
+
+  useEffect(() => {
+    if (selectedCountry) {
+      const { latlng } = selectedCountry.capitalInfo;
+      if (latlng && latlng.length === 2) {
+        const [latitude, longitude] = latlng;
+        const API = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${api_key}`;
+        axios
+          .get(API)
+          .then((response) => {
+            setWeatherData(response.data);
+          })
+          .catch((error) => {
+            console.error('Error fetching weather data:', error);
+            setWeatherData(null); 
+          });
+      }
+    }
+  }, [selectedCountry, api_key]);
 
   const filteredCountries = countries.filter((country) =>
     country.name.common.toLowerCase().includes(newText.toLowerCase())
   );
 
   const showCountry = (country) => {
-    setSelectedCountry(country); // Set the selected country when the button is clicked
+    setSelectedCountry(country);
   };
 
   return (
@@ -52,6 +74,15 @@ const App = () => {
             ))}
           </ul>
           <img src={selectedCountry.flags.png} alt={selectedCountry.name.common} />
+          {weatherData ? (
+            <div>
+              <p>Temperature: {weatherData.main.temp}°C</p>
+              <img src={`https://openweathermap.org/img/wn/${weatherData.weather?.[0]?.icon}@2x.png`} alt="Weather Icon" />
+              <p> wind {weatherData.wind.speed} m/s </p>
+            </div>
+          ) : (
+            <p>No weather data available</p>
+          )}
         </div>
       ) : filteredCountries.length > 10 ? (
         <p>Too many matches, specify another filter</p>
